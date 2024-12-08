7238e7be9cd6ad259cd539851e4675ead48b3528
Mouloud <mldrasd@gmail.com>
2024-12-08 19:02:49 +0100

    h

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: false,
+  reactStrictMode: true,
   images: {
     domains: ["m.media-amazon.com", "lh3.googleusercontent.com"],
   },
